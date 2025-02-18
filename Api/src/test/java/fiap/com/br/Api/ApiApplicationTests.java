ddc00a2be6904ebbcc6f993969b9a3e9ffb135ee
Lu1zEdu <le6269199@gmail.com>
2025-02-18 10:34:37 -0300

    18/02

---
+++ Api/src/test/java/fiap/com/br/Api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.com.br.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
