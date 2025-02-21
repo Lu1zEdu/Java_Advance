a8e41fb2953b79ee200a4837fc3ea5269307dc98
Lu1zEdu <le6269199@gmail.com>
2025-02-21 10:37:04 -0300

    Atualização

--- api_rest/src/main/java/java/br/com/fiap/api_rest/Dto/ClienteResponse.java
+++
@@ -1,4 +0,0 @@
-package java.br.com.fiap.api_rest.Dto;
-
-public record ClienteResponse (Long id, String nome) {
-}
--- api_rest/src/main/java/java/br/com/fiap/api_rest/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package java.br.com.fiap.api_rest.repository;
-
-import java.br.com.fiap.api_rest.models.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
