250a0d5040ce57c85090b2d049d372d83fc79e0e
Lu1zEdu <le6269199@gmail.com>
2025-03-14 11:45:11 -0300

    14/03

--- src/main/java/br/com/fiap/api_rest/model/Cliente.java
+++ src/main/java/br/com/fiap/api_rest/model/Cliente.java
@@ -4,24 +4,108 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private int idade;
+    private Date DatNas;
     private String email;
     private String senha;
     private String cpf;
     private Categoria categoria;
+    private boolean VIP;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, int idade, Date datNas, String email, String senha, String cpf, Categoria categoria, boolean VIP) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        DatNas = datNas;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.categoria = categoria;
+        this.VIP = VIP;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Date getDatNas() {
+        return DatNas;
+    }
+
+    public void setDatNas(Date datNas) {
+        DatNas = datNas;
+    }
+
+    public boolean isVIP() {
+        return VIP;
+    }
+
+    public void setVIP(boolean VIP) {
+        this.VIP = VIP;
+    }
 }
--- src/main/java/br/com/fiap/api_rest/repository/ClienteRepository.java
+++ src/main/java/br/com/fiap/api_rest/repository/ClienteRepository.java
@@ -4,6 +4,49 @@ import br.com.fiap.api_rest.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    List<Cliente> findByNome (String nome);
+    List<Cliente> findByNomels(String nome);
+    List<Cliente> FindByNomeEquals(String nome);
+    List<Cliente> findByNomeEqualsIgnoreCase(String nome);
+    List<Cliente> findByNameNot(String nome);
+    List<Cliente> findByNomeIsNot(String nome);
+    List<Cliente> findByNomeIsNotEquals(String nome);
+
+    List<Cliente> findTop3ByIdade(int idade);
+
+    List<Cliente> findByEmailIsNull();
+    List<Cliente> findByEmailIsNotNull();
+
+    //Idade
+    List<Cliente> findByIdade(int idade);
+    List<Cliente> findByIdadeGreaterThan(int idade);
+    List<Cliente> findByIdadeLessThan(int idade);
+    List<Cliente> findByIdadeBetween(int idadeStart, int idadeEnd);
+    List<Cliente> findByIdadeNot(int idade);
+
+    //Email
+    List<Cliente> findByEmail(String email);
+    List<Cliente> findByEmailIgnoreCase(String email);
+    List<Cliente> findByEmailNot(String email);
+    List<Cliente> findByEmailIsNot(String email);
+
+    //Vip
+    List<Cliente> findByVip(boolean vip);
+    List<Cliente> findByVipTrue();
+    List<Cliente> findByVipFalse();
+    List<Cliente> findByVipNot(boolean vip);
+
+    //Data de Nascimento
+    List<Cliente> findByDtanasc(Date dtanasc);
+    List<Cliente> findByDtanascBefore(Date dtanasc);
+    List<Cliente> findByDtanascAfter(Date dtanasc);
+    List<Cliente> findByDtanascBetween(Date startDate, Date endDate);
+    List<Cliente> findByDtanascIsNull();
+    List<Cliente> findByDtanascIsNotNull();
+
 }
